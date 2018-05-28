f3dbc97c6c539111d0f3b99f17279bf11ec666b4
ABHISHEK THOMBRE <ug201314001@iitj.ac.in>
2018-05-28 17:08:10 +0530

    Update build.py

--- q02_twitter_data/build.py
+++ q02_twitter_data/build.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.Twitter_sentiment_analysis_project.q01_twitter_setup.build import q01_twitter_setup
+from Twitter_sentiment_analysis_project.q01_twitter_setup.build import q01_twitter_setup
 
 
 def q02_twitter_data():
