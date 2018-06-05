814f10556e887fa6df3465ee1821ae6453aa8a44
ABHISHEK THOMBRE <ug201314001@iitj.ac.in>
2018-06-05 12:45:44 +0530

    Update build.py

--- q02_twitter_data/build.py
+++ q02_twitter_data/build.py
@@ -1,11 +1,6 @@
-
-import os
-import sys
-
 import numpy as np
 import pandas as pd
 
-sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from Twitter_sentiment_analysis_project.q01_twitter_setup.build import q01_twitter_setup
 
 
